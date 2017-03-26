82ccd198d41a8323c7b9d56a5d6e6fb5a8a82f57
Joseph Nicholas R. Alcantara <josephnicholasr.alcantara@josephs-macbook-pro.local>
2017-03-26 16:16:35 +0800

    Initial Commit

---
+++ WatchApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WatchApp
+//
+//  Created by Joseph Nicholas R. Alcantara on 26/03/2017.
+//  Copyright © 2017 Joseph Nicholas R. Alcantara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
